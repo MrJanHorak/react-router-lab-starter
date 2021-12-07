4f9ac95eec01ab02e2b446a476ea0247b0124065
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-07 15:02:30 -0500

    add App.css file and minimal styling

--- src/components/App.js
+++ src/components/App.js
@@ -1,18 +1,10 @@
 import React from "react";
-import Home from "./Home";
 import{ Link, Outlet, NavLink } from 'react-router-dom'
-
+import './App.css';
 const App = () => {
     return (
         <div>
-          <nav         style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
-          display: "flex",
-          gap: "15px",
-          fontSize: "20px",
-
-        }}>
+          <nav>        
             <NavLink to="/">Go to Home Page</NavLink>
             <NavLink to="/procedures">See Our Procedures</NavLink>
             <NavLink to="/contact">Contact Us!</NavLink>

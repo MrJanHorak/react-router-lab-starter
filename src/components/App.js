ba71db2917d099748cd3ad30e8d8b2ca2636bf9e
MrJanHorak <janny_b_goode@hotmail.com>
2021-12-07 15:15:29 -0500

    tweak css for visited links

--- src/components/App.js
+++ src/components/App.js
@@ -1,17 +1,46 @@
 import React from "react";
-import{ Link, Outlet, NavLink } from 'react-router-dom'
-import './App.css';
+import { Link, Outlet, NavLink } from "react-router-dom";
+import "./App.css";
 const App = () => {
-    return (
-        <div>
-          <nav>        
-            <NavLink to="/">Go to Home Page</NavLink>
-            <NavLink to="/procedures">See Our Procedures</NavLink>
-            <NavLink to="/contact">Contact Us!</NavLink>
-          </nav>
-          <Outlet />
-          </div>
-    );
-}
+  return (
+    <div>
+      <nav>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+
+              color: isActive ? "red" : "",
+            };
+          }}
+          to="/"
+        >
+          Go to Home Page
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+
+              color: isActive ? "red" : "",
+            };
+          }}
+          to="/procedures"
+        >
+          See Our Procedures
+        </NavLink>
+        <NavLink
+          style={({ isActive }) => {
+            return {
+              color: isActive ? "red" : "",
+            };
+          }}
+          to="/contact"
+        >
+          Contact Us!
+        </NavLink>
+      </nav>
+      <Outlet />
+    </div>
+  );
+};
 
 export default App;
